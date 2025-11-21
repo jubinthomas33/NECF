a5ae7a03cb5f39c722ea5c34c465da1090154795
jubinthomas33 <jubinthomas33@gmail.com>
2025-11-21 14:25:14 +0530

    Config error rectified

--- NECF/src/test/java/com/utility/ConfigReader.java
+++ NECF/src/test/java/com/utility/ConfigReader.java
@@ -1,28 +1,31 @@
 package com.utility;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigReader {
-	private static Properties prop;
+    private static Properties prop;
 
-	public static void loadProperties() {
-		prop = new Properties();
-		try {
-			FileInputStream fis = new FileInputStream("src/test/Resources/Config.properties");
-			prop.load(fis);
-		} catch (IOException e) {
-			e.printStackTrace();
-			throw new RuntimeException("Failed to load config.properties file!");
-		}
+    public static void loadProperties() {
+        try {
+            prop = new Properties();
+            InputStream is = ConfigReader.class.getClassLoader()
+                    .getResourceAsStream("Config.properties");
 
-	}
+            if (is == null) {
+                throw new RuntimeException("config.properties NOT FOUND in classpath!");
+            }
 
-	public static String getProperty(String key) {
-		if (prop == null) {
-			loadProperties();
-		}
-		return prop.getProperty(key);
-	}
+            prop.load(is);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load config.properties!", e);
+        }
+    }
+
+    public static String getProperty(String key) {
+        if (prop == null) {
+            loadProperties();
+        }
+        return prop.getProperty(key);
+    }
 }
